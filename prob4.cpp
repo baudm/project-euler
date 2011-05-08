9b823235f8b574f1218f31c9fc77807c36839b44
Darwin Bautista <daruuin@gmail.com>
2011-05-08 23:09:57 +0800

    Add solution to problem 4

---
+++ prob4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+
+unsigned reverse_int(unsigned n)
+{
+	unsigned r = 0, u;
+
+	while (n) {
+		u = n % 10;
+		n /= 10;
+		r = r * 10 + u;
+	}
+
+	return r;
+}
+
+
+bool is_palindrome(unsigned n)
+{
+	return (n == reverse_int(n));
+}
+
+int main(void)
+{
+	unsigned a, b, p = 0;
+
+	for (a = 999; a >= 100; a--) {
+		for (b = a; b >= 100; b--) {
+			if (is_palindrome(a * b) && a * b > p)
+				p = a * b;
+		}
+	}
+
+	cout << "A = " << a << ", B = " << b << ";" << p << endl;
+
+	return 0;
+}
