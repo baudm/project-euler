9b480e4a98b58d119ba7d23573a074c5d0642008
Darwin Bautista <daruuin@gmail.com>
2011-05-09 10:47:38 +0800

    Add solution to Problem 7

---
+++ prob7.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool is_prime(unsigned n)
+{
+	unsigned k, p = 0, s = sqrt(n);
+
+	if ((n % 2 == 0 && n != 2) || (n % 3 == 0 && n != 3))
+		return false;
+
+	for (k = 1; p <= s; k++) {
+		p = 6 * k + 1;
+		if ((n % p == 0 || n % (p - 2) == 0) && (n / p != 1 && n / (p - 2) != 1))
+			return false;
+	}
+	return true;
+}
+
+
+int main(void)
+{
+	int i = 0, p = 1;
+
+	while (i < 10001) {
+		if (is_prime(++p))
+			i++;
+	}
+
+	cout << p << endl;
+
+	return 0;
+}
