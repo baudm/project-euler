16662d4cb8b151ac7f4868486d291a38cafc1001
Darwin Bautista <daruuin@gmail.com>
2011-06-17 10:22:53 +0800

    Refactor solution to problem 15 using combinations instead of generating a triangle

--- prob015.py
+++ prob015.py
@@ -1,21 +1,20 @@
 
+def factorial(n):
+	prod = 1
+	while n > 1:
+		prod *= n
+		n -= 1
+	return prod
 
-# From: http://code.activestate.com/recipes/577542-pascals-triangle/
-def pascals_triangle(n):
-    x=[[1]]
-    for i in range(n-1):
-        x.append(list(map(sum,zip([0]+x[-1],x[-1]+[0]))))
-    # HACK: only last row is needed
-    return x.pop()
+
+def C(n, k):
+	return factorial(n) / (factorial(k) * factorial(n - k))
 
 
 def get_num_routes(n):
 	# Get the corresponding Pascal's triangle row number
-	row_num = 2*n + 1
-	row = pascals_triangle(row_num)
-	# Return the middle term
-	return row[len(row)/2]
-
+	row_num = 2*n
+	return C(row_num - 1, row_num / 2)
 
 if __name__ == '__main__':
 	# n = 20 for 20x20 grid
