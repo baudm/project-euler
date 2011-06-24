7b71be34af95364ab29f9b8e0335b6a4222278f5
Darwin Bautista <daruuin@gmail.com>
2011-06-24 15:07:18 +0800

    Refactor solution to problem 28

--- 028.cpp
+++ 028.cpp
@@ -6,16 +6,14 @@ int main(void)
 {
 	unsigned long sum = 0;
 	unsigned num = 1;
-	unsigned short i, diff = 2;
+	unsigned short diff = 2;
 	const unsigned limit = 1001 * 1001;
 
 	sum += num;
 	while (num < limit) {
 		// four corners of each 'layer' of the spiral
-		for (i = 0; i < 4; i++) {
-			num += diff;
-			sum += num;
-		}
+		sum += 10 * diff + 4 * num;
+		num += 4 * diff;
 		diff += 2;
 	}
 
