54b295cb5818db44b27b6739aa3566f89fa34b79
Darwin Bautista <daruuin@gmail.com>
2011-06-29 11:54:22 +0800

    Add fib() template

--- euler.hpp
+++ euler.hpp
@@ -1,4 +1,23 @@
 namespace euler
 {
-	bool is_prime(uint64_t num);
+	// Reference: http://oeis.org/A000045
+	template <typename T, typename U>
+	T fib(U n)
+	{
+		if (n == 0)
+			return 0;
+
+		T tmp, prev = 0, curr = 1;
+
+		while (n > 1) {
+			tmp = curr;
+			curr += prev;
+			prev = tmp;
+			n--;
+		}
+
+		return curr;
+	}
+
+	extern bool is_prime(uint64_t num);
 }
