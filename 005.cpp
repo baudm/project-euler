7530f1f4d47be5834e5c39856409362a45ae32ed
Darwin Bautista <daruuin@gmail.com>
2011-06-30 23:08:24 +0800

    Refactor problem 5 solution to use libeuler

--- 005.cpp
+++ 005.cpp
@@ -1,28 +1,13 @@
 #include <iostream>
+#include "euler.hpp"
 
-
-unsigned long gcf(unsigned long a, unsigned long b)
-{
-	while (a && b) {
-		if (a > b)
-			a %= b;
-		else
-			b %= a;
-	}
-	return (a) ? a : b;
-}
-
-unsigned long lcm(unsigned long a, unsigned long b)
-{
-	return a * (b / gcf(a, b));
-}
 
 int main(void)
 {
 	unsigned long i, l = 1;
 
 	for (i = 2; i <= 20; i++)
-		l = lcm(l, i);
+		l = euler::lcm(l, i);
 
 	std::cout << l << std::endl;
 
