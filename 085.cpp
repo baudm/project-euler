22e8f22a18d768c239aeef5478a3f81e72223daa
Darwin Bautista <daruuin@gmail.com>
2011-07-20 17:26:29 +0800

    Add final solution for problem 85

--- 085.cpp
+++ 085.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdint>
 #include <cmath>
-
+#include "euler.hpp"
 
 int main(void)
 {
@@ -9,25 +9,19 @@ int main(void)
 	uint32_t count, diff, min_diff = 100000;
 	uint32_t best_area;
 	uint16_t width, height;
-	uint16_t w, h;
 
-	for (width = 1; width <= 5; width++) {
-		for (height = 1; height <= 5; height++) {
-			count = 0;
-			for (w = width; w > 0; w--) {
-				for (h = height; h > 0; h--)
-					count += (width - w + 1) * (height - h + 1s);
-			}
+	for (width = 20; width <= 85; width++) {
+		for (height = 20; height <= 85; height++) {
+			count = euler::triangular<uint32_t>(width) * euler::triangular<uint32_t>(height);
 			diff = (target > count) ? target - count : count - target;
 			if (diff < min_diff) {
 				min_diff = diff;
 				best_area = width*height;
 			}
-			std::cout << width << "x" << height << " = " << count << std::endl;
 		}
 	}
 
-	std::cout << min_diff << " " << best_area << std::endl;
+	std::cout << best_area << std::endl;
 
 	return 0;
 }
