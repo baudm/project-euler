079472d96998cbbf332ada06c018b75dc4b31fef
Darwin Bautista <daruuin@gmail.com>
2011-08-08 10:27:10 +0800

    Refactor 049.cpp to use libeuler functions

--- 049.cpp
+++ 049.cpp
@@ -15,39 +15,6 @@ void get_digits(uint8_t digits[], uint16_t num)
 }
 
 
-uint16_t get_value(uint8_t digits[])
-{
-	return 1000 * digits[0] + 100 * digits[1] + 10 * digits[2] + digits[3];
-}
-
-
-bool is_permutation(uint16_t a, uint16_t b)
-{
-	uint8_t count[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	uint16_t nums[] = {a, b};
-	uint8_t digit, i;
-
-	for (i = 0; i < 2; i++) {
-		while (nums[i]) {
-			digit = nums[i] % 10;
-			count[digit]++;
-			nums[i] /= 10;
-		}
-	}
-
-	/**
-	 * If a and b are permutations of each other, the count for each digit
-	 * should be even.
-	 */
-	for (i = 0; i < 10; i++) {
-		if (count[i] % 2 != 0)
-			return false;
-	}
-
-	return true;
-}
-
-
 int main(void)
 {
 	uint16_t p, diff, next;
@@ -60,7 +27,7 @@ int main(void)
 			continue;
 		get_digits(digits, p);
 		while (std::next_permutation(digits, digits + 4)) {
-			next = get_value(digits);
+			next = euler::digits2int<uint16_t>(10, digits, digits + 4);
 			if (euler::is_prime(next)) {
 				diff = next - p;
 				next += diff;
@@ -68,7 +35,7 @@ int main(void)
 				 * The third term of the sequence should also be
 				 * a permutation of p and should also be prime.
 				 */
-				if (is_permutation(p, next) && euler::is_prime(next)) {
+				if (euler::is_permutation(p, next) && euler::is_prime(next)) {
 					finished = true;
 					break;
 				}
